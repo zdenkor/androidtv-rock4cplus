68ab985581c5fd5f120af625a8fb497f03f2021d
acharis <acharis@users.noreply.github.com>
2026-05-31 21:54:00 +0200

    Remove obsolete fix_apps.sh

--- scripts/fix_apps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "c:/Temp/AndroidTV for Radxa4C+"
-sed -i 's/||||Projectivy.apk/|||||Projectivy.apk/' scripts/03a-preinstall-apps.sh
-sed -i 's/||||TVBro.apk/|||||TVBro.apk/' scripts/03a-preinstall-apps.sh
-sed -i 's/||||ButtonMapper.apk/|||||ButtonMapper.apk/' scripts/03a-preinstall-apps.sh
-sed -i 's/||||TiviMate.apk/|||||TiviMate.apk/' scripts/03a-preinstall-apps.sh
-sed -i 's/||||Xplore.apk/|||||Xplore.apk/' scripts/03a-preinstall-apps.sh
-sed -i 's/||||SideloadLauncher.apk/|||||SideloadLauncher.apk/' scripts/03a-preinstall-apps.sh
-sed -i 's/||||AptoideTV.apk/|||||AptoideTV.apk/' scripts/03a-preinstall-apps.sh
